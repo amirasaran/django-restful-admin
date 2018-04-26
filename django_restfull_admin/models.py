55df37f646edcc042c51d6fce5a6f3af493c8fb1
Amir Asaran <admin@mihanmail.com>
2018-04-26 18:11:22 +0430

    initial commit

---
+++ django_restfull_admin/models.py
@@ -0,0 +1,158 @@
+from django.db.models.base import ModelBase
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.routers import DefaultRouter
+from rest_framework.serializers import ModelSerializer
+
+
+class AlreadyRegistered(Exception):
+    pass
+
+
+class NotRegistered(Exception):
+    pass
+
+
+class ImproperlyConfigured(Exception):
+    pass
+
+
+class RestFullModelAdmin(viewsets.ModelViewSet):
+    queryset = None
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def create(self, request, **kwargs):
+        return super().create(request, **kwargs)
+
+    def retrieve(self, request, pk=None, **kwargs):
+        return super().retrieve(request, pk=pk, **kwargs)
+
+    def update(self, request, pk=None, **kwargs):
+        return super().update(request, pk=pk, **kwargs)
+
+    def partial_update(self, request, pk=None, **kwargs):
+        return super().partial_update(request, pk=pk, **kwargs)
+
+    def destroy(self, request, pk=None, **kwargs):
+        return super().destroy(request, pk=pk, **kwargs)
+
+
+
+class BaseModelSerializer(ModelSerializer):
+    class Meta:
+        pass
+
+
+class RestFullAdminSite:
+    def __init__(self):
+        self._registry = {}
+
+    def register(self, model_or_iterable, view_class=None, **options):
+        if not view_class:
+            view_class = RestFullModelAdmin
+
+        if isinstance(model_or_iterable, ModelBase):
+            model_or_iterable = [model_or_iterable]
+        for model in model_or_iterable:
+            if model._meta.abstract:
+                raise ImproperlyConfigured(
+                    'The model %s is abstract, so it cannot be registered with admin.' % model.__name__
+                )
+
+            if model in self._registry:
+                raise AlreadyRegistered('The model %s is already registered' % model.__name__)
+            options.update({
+                "__doc__": self.generate_docs(model),
+            })
+            view_class = type("%sAdmin" % model.__name__, (view_class,), options)
+
+            # Instantiate the admin class to save in the registry
+            self._registry[model] = view_class
+
+    def unregister(self, model_or_iterable):
+        """
+        Unregister the given model(s).
+
+        If a model isn't already registered, raise NotRegistered.
+        """
+        if isinstance(model_or_iterable, ModelBase):
+            model_or_iterable = [model_or_iterable]
+        for model in model_or_iterable:
+            if model not in self._registry:
+                raise NotRegistered('The model %s is not registered' % model.__name__)
+            del self._registry[model]
+
+    def is_registered(self, model):
+        """
+        Check if a model class is registered with this `AdminSite`.
+        """
+        return model in self._registry
+
+    def get_urls(self):
+        router = DefaultRouter()
+        view_sets = []
+        for model, view_set in self._registry.items():
+            if view_set.queryset is None:
+                view_set.queryset = model.objects.all()
+            if view_set.serializer_class is None:
+                serializer_class = type("%sModelSerializer" % model.__name__, (ModelSerializer,), {
+                    "Meta": type("Meta", (object,), {
+                        "model": model,
+                        "fields": "__all__"
+                    }),
+                })
+                view_set.serializer_class = serializer_class
+
+            view_sets.append(view_set)
+            router.register('%s/%s' % (model._meta.app_label, model._meta.model_name), view_set)
+
+        return router.urls
+
+    def generate_docs(self, model):
+        return """
+###The APIs include:
+
+
+> `GET`  {app}/{model} ===> list all `{verbose_name_plural}` page by page;
+
+> `HEAD`  {app}/{model} ===> show the overview information of `{verbose_name}` listing
+
+> `HEAD`  {app}/{model} ===> show the overview information of `{verbose_name}` listing
+
+> `POST`  {app}/{model} ===> create a new `{verbose_name}`
+
+> `GET` {app}/{model}/123 ===> return the details of the `{verbose_name}` 123
+
+> `HEAD` {app}/{model}/123 ===> show the overview information of `{verbose_name}` 123
+
+> `PATCH` {app}/{model}/123 and `PUT` {app}/{model}/123 ==> update the `{verbose_name}` 123
+
+> `DELETE` {app}/{model}/123 ===> delete the `{verbose_name}` 123
+
+> `OPTIONS` {app}/{model} ===> show the supported verbs regarding endpoint `{app}/{model}`
+
+> `OPTIONS` {app}/{model}/123 ===> show the supported verbs regarding endpoint `{app}/{model}/123`
+
+        """.format(
+            app=model._meta.app_label,
+            model=model._meta.model_name,
+            verbose_name=model._meta.verbose_name,
+            verbose_name_plural=model._meta.verbose_name_plural
+        )
+
+    @property
+    def urls(self):
+        return self.get_urls(), 'django_restfull_admin', 'django_restfull_admin'
+
+
+site = RestFullAdminSite()
---
+++ setup.py
@@ -0,0 +1,53 @@
+"""
+See:
+https://github.com/amirasaran/djang-restfull-admin
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='django-restfull-admin',
+    version='1.0.0',
+    description='Python Django RestFull Admin',
+    long_description=long_description,
+    url='https://github.com/amirasaran/djang-restfull-admin',
+    author='Amir Mohsen Asaran',
+    author_email='admin@mihanmail.com',
+    license='MIT',
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    packages=['django_restfull_admin'],
+    # What does your project relate to?
+    keywords='dajngo restfull admin',
+)
